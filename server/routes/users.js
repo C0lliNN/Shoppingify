fe8cb1fdc123b07a51bff7031ab8cf6a2dfb4d8d
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-08-25 14:21:07 -0300

    Create Routes and Test for User Registration

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const { validateUser, User } = require('../models/User');
+const debug = require('debug')('users');
 const router = express.Router();
+
+router.post('/', async (request, response) => {
+  const { value, error } = validateUser(request.body);
+  if (error) {
+    return response.status(400).send({ message: error.message });
+  }
+
+  try {
+    const user = await User.create(value);
+    response
+      .status(201)
+      .send({ _id: user._id, name: user.name, email: user.email });
+  } catch (error) {
+    debug(error.message);
+    response.status(500).send(error.message);
+  }
+});
 
 module.exports = router;
--- server/routes/users.test.js
+++ server/routes/users.test.js
@@ -1,4 +1,59 @@
 /* eslint-disable no-undef */
+const request = require('supertest');
+const { start, stop } = require('../config/server');
+const { initDatabase, dropDatabase } = require('../config/database');
+
+let app = null;
+
+beforeEach(async () => {
+  app = await start();
+  await initDatabase();
+});
+
+afterEach(async () => {
+  stop();
+  await dropDatabase();
+});
+
 test('It should return true', () => {
   expect(true).toBe(true);
+});
+
+describe('POST /users', () => {
+  // Payload specif validation is already tested in models/user.test.js
+  it('should send 400 if the payload is invalid', async () => {
+    await request(app)
+      .post('/api/v1/users')
+      .send({ name: 'Raphael', password: '9565268953' })
+      .expect(400);
+  });
+
+  it('should send the error message if the payload is invalid', async () => {
+    const { body } = await request(app)
+      .post('/api/v1/users')
+      .send({ name: 'Raphael', password: '9565268953' })
+      .expect(400);
+    expect(typeof body).toBe('object');
+    expect(body.message).toBeTruthy();
+  });
+
+  it('should send 201 if payload is valid', async () => {
+    await request(app)
+      .post('/api/v1/users')
+      .send({ name: 'Raphael', email: 'test@test.com', password: '888975279' })
+      .expect(201);
+  });
+
+  it('should send user object if payload is valid', async () => {
+    const { body } = await request(app)
+      .post('/api/v1/users')
+      .send({ name: 'Raphael', email: 'test@test.com', password: '888975279' })
+      .expect(201);
+
+    expect(typeof body).toBe('object');
+    expect(body._id).toBeTruthy();
+    expect(body.name).toBeTruthy();
+    expect(body.password).toBeFalsy();
+    expect(body.email).toBeTruthy();
+  });
 });
