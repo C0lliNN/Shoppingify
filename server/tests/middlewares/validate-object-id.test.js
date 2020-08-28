f0d86ad20339ad0d301c5219295b6cc9504292be
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2020-08-28 19:04:54 -0300

    Update Validate ObjectId Tests

--- server/tests/middlewares/validate-object-id.test.js
+++ server/tests/middlewares/validate-object-id.test.js
@@ -1,7 +1,66 @@
 /* eslint-disable no-undef */
+const { start, stop } = require('../../startup/server');
+const { initDatabase, dropDatabase } = require('../../startup/database');
+const request = require('supertest');
+const { User } = require('../../models/User');
+const { List } = require('../../models/list');
+
+let app;
+
+beforeEach(async () => {
+  app = await start();
+  await initDatabase();
+});
+
+afterEach(async () => {
+  stop();
+  await dropDatabase();
+});
 
 describe('validate-object-id middleware', () => {
-  it('should return true', () => {
-    expect(true).toBe(true);
+  let user = null;
+  beforeEach(() => {
+    user = new User({
+      name: 'Raphael Collin',
+      email: 'test@test.com',
+      password: '111111111',
+    });
+  });
+  it('should send 404 if the id is invalid', async () => {
+    await request(app)
+      .patch('/api/v1/lists/123/complete')
+      .set('Authorization', `Bearer ${user.generateToken()}`)
+      .expect(404);
+  });
+  it('should send 200 if it is a valid objectId', async () => {
+    const list = await List.create({
+      name: 'October 2020 List',
+      itens: [
+        {
+          _id: '5f455552f75a6016403b9971',
+          name: 'Banana',
+          category: {
+            _id: '5f455552f75a6016403b9971',
+            name: 'Fruit',
+          },
+          quantity: 3,
+        },
+        {
+          _id: '5f455552f75a6016403b9972',
+          name: 'Apple',
+          category: {
+            _id: '5f455552f75a6016403b9971',
+            name: 'Fruit',
+          },
+          quantity: 4,
+        },
+      ],
+      status: 'active',
+      user: user.id,
+    });
+    await request(app)
+      .patch(`/api/v1/lists/${list.id}/complete`)
+      .set('Authorization', `Bearer ${user.generateToken()}`)
+      .expect(200);
   });
 });
